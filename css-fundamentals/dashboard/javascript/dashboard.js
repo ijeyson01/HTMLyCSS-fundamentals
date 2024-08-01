f15b0a8bb897337f8e1d0a05ee2b00a215314a4a
iJeyson <jeyson867@gmail.com>
2024-08-01 17:52:52 -0500

    INTRODUCCION A JS

---
+++ css-fundamentals/dashboard/javascript/dashboard.js
@@ -0,0 +1,20 @@
+var variable = 1;
+console.log(variable + 1)
+variable = 'Hola';
+console.log(variable + ' mundo!');
+variable = true;
+
+console.log(variable);
+const varConstante = 'hola';
+
+console.log(varConstante);
+
+
+function obtenerTextoBuscar(){
+    let valBuscar = document.getElementById('buscar');
+    window.alert(valBuscar.value);
+}
+
+function saludo(){
+    
+}
