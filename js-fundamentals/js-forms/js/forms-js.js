dcccbe1b839708b83f0d51ca9a9e0a0760fcbe2f
iJeyson <jeyson867@gmail.com>
2024-08-03 10:54:29 -0500

    FORMULARIO VALIDACIONES

--- js-fundamentals/js-forms/js/forms-js.js
+++ js-fundamentals/js-forms/js/forms-js.js
@@ -1,10 +1,47 @@
+const toastAlertaInicioSesion = document.getElementById('alertaInicioSesion')
+const usuarios = [
+    {
+        "usuario": "Juan1234",
+        "email": "eljuan2024@mail.com",
+        "password": "123456",
+        "tipousuario": "ADMIN"
+    },
+    {
+        "usuario": "Jesus001",
+        "email": "elyisus@mail.com",
+        "password": "123456",
+        "tipousuario": "ADMIN"
+    },
+    {
+        "usuario": "DavidDev",
+        "email": "ddev@mail.com",
+        "password": "123456",
+        "tipousuario": "USER"
+    }
+];
+
+
 function eventoForm(){
     let email = document.getElementById('email');
     let password = document.getElementById('password');
-    if(email.value != "" && password.value != ""){
-        alert("Su email es: " + email.value + " Su contraseña es: " + password.value);
+    let usuarioEmail = usuarios.find( x => x.email == email.value);
+    if(usuarioEmail == undefined){
+        mostrarAlerta();
+    } else {
+        if(usuarioEmail['password'] == password.value) {
+            window.location.href('../../js-list-table/page/list/table.html');
+        } else {
+            mostrarAlerta();
+        }
+    }
+    /*if(email.value != "" && password.value != ""){
+       
     }else{
         alert("Los campos no pueden estar vacio.");
-    }
+    }*/
+}
 
+function mostrarAlerta () {
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastAlertaInicioSesion)
+    toastBootstrap.show()
 }
---
+++ js-fundamentals/js-list-table/js/lis-table.js
@@ -0,0 +1,20 @@
+const usuarios = [
+    {
+        "usuario": "Juan1234",
+        "email": "juan2024@mail.com",
+        "password": "123456",
+        "tipousuario": "ADMIN"
+    },
+    {
+        "usuario": "Jesus001",
+        "email": "elyisus@mail.com",
+        "password": "123456",
+        "tipousuario": "ADMIN"
+    },
+    {
+        "usuario": "DavidDev",
+        "email": "ddev@mail.com",
+        "password": "123456",
+        "tipousuario": "USER"
+    }
+];
